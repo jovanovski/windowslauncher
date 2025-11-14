cc922bb62a32ee01b402d4be8cdf2cd6649c331d
Gorjan Jovanovski <hey@gorjan.rocks>
2025-11-14 15:49:11 +0100

    Increment version number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "rocks.gorjan.gokixp"
         minSdk = 30
         targetSdk = 36
-        versionCode = 13
-        versionName = "1.8.4"
+        versionCode = 14
+        versionName = "1.8.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
